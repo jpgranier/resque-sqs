e7802dfc7e4b814935bc8d804798047361d71851
Jonathan Granier <jgranier99@gmail.com>
2023-06-11 06:46:02 -0500

    Fix unit tests for resque_sqs_failure_multiple_test

--- test/resque_sqs_failure_multiple_test.rb
+++ test/resque_sqs_failure_multiple_test.rb
@@ -6,8 +6,10 @@ describe 'ResqueSqs::Failure::Multiple' do
     with_failure_backend(ResqueSqs::Failure::Multiple) do
       ResqueSqs::Failure::Multiple.classes = [ResqueSqs::Failure::Redis]
       exception = StandardError.exception('some error')
+      ResqueSqs.data_store.sqs.add_queue(:test)
       worker = ResqueSqs::Worker.new(:test)
       payload = { 'class' => 'Object', 'args' => 3 }
+      ResqueSqs.data_store.sqs.add_queue(:queue)
       ResqueSqs::Failure.create({:exception => exception, :worker => worker, :queue => 'queue', :payload => payload})
       ResqueSqs::Failure::Multiple.requeue_all # should not raise an error
     end
@@ -16,6 +18,7 @@ describe 'ResqueSqs::Failure::Multiple' do
   it 'requeue_queue delegates to the first class and returns a mapped queue name' do
     with_failure_backend(ResqueSqs::Failure::Multiple) do
       mock_class = MiniTest::Mock.new
+      ResqueSqs.data_store.sqs.add_queue(:requeue_queue)
       mock_class.expect(:requeue_queue, 'mapped_queue', ['queue'])
       ResqueSqs::Failure::Multiple.classes = [mock_class]
       assert_equal 'mapped_queue', ResqueSqs::Failure::Multiple.requeue_queue('queue')
@@ -31,6 +34,7 @@ describe 'ResqueSqs::Failure::Multiple' do
 
       ResqueSqs::Failure::Multiple.classes = [mock]
       ResqueSqs::Failure::Multiple.remove(1, :test_queue)
+      ResqueSqs.data_store.sqs.add_queue(:test_queue)
       assert_equal :test_queue, mock.instance_variable_get('@queue')
     end
   end
